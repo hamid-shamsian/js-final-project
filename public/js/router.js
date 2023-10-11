fca92ef2937f913ce4960240b68cd7aefcb2f2f0
Hamid Shamsian <hamid64bits@gmail.com>
2023-10-11 18:07:51 +0400

    refactor: 404 routing functionality refactored agian to improve performance.

--- public/js/router.js
+++ public/js/router.js
@@ -15,21 +15,22 @@ const routes = {
 const spinner = document.getElementById("spinner");
 
 export const navigateTo = input => {
+  if (input == 404) return RenderApp(true);
+
   input.preventDefault?.(); // input has 2 valid types: maybe a normal string containing the relative path to go, or maybe an event object generated from clicking an anchor (a) element associated with a click event listener.
   const path = input.currentTarget?.href ?? input; // 👈 above comment 👆   and currentTarget instead of target: because some <a> tags may have children like icons or spans...
-  if (path == "404") RenderApp("404");
-  else {
-    window.history.pushState({}, "", path);
-    RenderApp();
-  }
+
+  window.history.pushState({}, "", path);
+  RenderApp();
 };
 
-const RenderApp = async notToBePushedPath => {
+const RenderApp = async is404 => {
   spinner.style.display = "";
 
-  const path = notToBePushedPath ?? window.location.pathname; // when notToBePushedPath string is passed to function, then this path is only about to be rendered but the url of the browser is not gonna be changed (for example: loading 404 page when a wrong url is entered by user.)
+  const path = is404 ? "404" : window.location.pathname; // when a true is404 argument (or any truthy value) is passed to function, then 404 page is only about to be rendered but the url of the browser is not gonna be changed (the url should not change for loading 404 page when a wrong url is entered by user.)
   const user = User.get();
 
+  // Protecting Routs: ==========================
   if (path == "/" || path == "/login") {
     if (user) {
       showToast("You are already Signed in!"); // redirect signed-in users from onboarding or login pages to home page.
@@ -42,11 +43,12 @@ const RenderApp = async notToBePushedPath => {
     showToast("Your Cart is Empty!", "orangered"); // protect checkout page against signed-in users with no cart item.
     return navigateTo("/home");
   }
+  // ============================================
 
   const { title, fileName, noJS } = routes[path] ?? routes[404];
-
   document.title = title;
 
+  // Fetching Resources of the page: ============
   try {
     const template = await fetch(`../templates/${fileName}.html`);
     const html = await template.text();
@@ -60,6 +62,7 @@ const RenderApp = async notToBePushedPath => {
   } finally {
     spinner.style.display = "none";
   }
+  // ============================================
 };
 
 window.onpopstate = e => RenderApp(); // to make Back and Forward buttons of the browser still working... (i used arrow function instead of simply assigning RenderApp function reference because onpopstate event on window passes an event object to the handler function and this will interfere with the RenderApp parameter functionality.)
